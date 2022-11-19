ff3471c81d86a458f88be5519f442e15aae946dd
mme <9083787+mme022@users.noreply.github.com>
2022-11-19 10:49:47 +0000

    init

---
+++ hardhat.config.ts
@@ -0,0 +1,79 @@
+require("./tasks")
+
+import "@typechain/hardhat"
+import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-ethers"
+import "@nomicfoundation/hardhat-toolbox"
+import "hardhat-gas-reporter"
+import "dotenv/config"
+import "solidity-coverage"
+import "hardhat-deploy"
+import { HardhatUserConfig } from "hardhat/config"
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "privatKey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+
+const config: HardhatUserConfig = {
+    defaultNetwork: "hardhat",
+    networks: {
+        wallaby: {
+            url: "https://wallaby.node.glif.io/rpc/v0",
+            accounts: [PRIVATE_KEY],
+        },
+        hardhat: {
+            chainId: 31337,
+            allowUnlimitedContractSize: true,
+        },
+        localhost: {
+            chainId: 31337,
+            allowUnlimitedContractSize: true,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+    },
+    solidity: {
+        version: "0.8.9",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+    },
+    mocha: {
+        timeout: 200000, // 200 seconds max for running tests
+    },
+    paths: {
+        sources: "./contracts",
+        tests: "./test",
+        cache: "./cache",
+        artifacts: "./artifacts",
+    },
+}
+
+export default config
